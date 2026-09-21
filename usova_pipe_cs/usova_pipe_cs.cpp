9c72dddd419bc20d522b340fee026127aab72e22
user <user@DESKTOP-6HGS7ND>
2026-09-21 21:57:29 +0300

    v.1.0

---
+++ usova_pipe_cs/usova_pipe_cs.cpp
@@ -0,0 +1,26 @@
+﻿﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = -1;
+
+    while (x != 0) {
+        cout << "Menu\n";
+        cout << "1. Add a pipe\n";
+        cout << "2. Add CS\n";
+        cout << "3. Viewing all objects\n";
+        cout << "4. Edit the pipe\n";
+        cout << "5. Eit the CS \n";
+        cout << "6. Save\n";
+        cout << "7. Download\n";
+        cout << "0. Exit\n";
+        cin >> x;
+
+        if (x == 1) {
+            cout << "Pipe";
+            break;
+
+        }
+    }
+}
